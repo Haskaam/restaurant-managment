8332d5247e317b43300d41bae7079d75a43d99ec
Haskaam <kuba.stasinski@wp.pl>
2026-07-23 16:16:26 +0200

    Design user managment database structure

--- database/migrations/2026_07_23_113423_create_users_table.php
+++ database/migrations/2026_07_23_113423_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            $table->foreignId('department_id')
+            ->nullable()
+            ->constrained()
+            ->nullOnDelete();
+
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->boolean('must_change_password')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('employment_ended_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
